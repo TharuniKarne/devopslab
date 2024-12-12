c79dd0d80c15d410a81f219f1051cd39879eec8d
TharuniKarne <ucejcse31@gmail.com>
2024-12-12 14:50:42 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,15 @@
+public class AddTwoNumbers {
+       public static void main(String[] args){
+       if(args.length <2) {
+       System.out.println("Please provide  two numbers as arguments.");
+       return ;
+       }
+       //Parse the numbers from command-Line arguments
+       int num1 = Integer.parseInt(args[0]);
+       int num2 = integer.parseInt(args[1]);
+       //calculate the sum
+       int sum = num1+num2;
+       //Display the result
+       System.out.println("The sum of "+num1+"and"+num2+"is:"+sum);
+       }
+       }
